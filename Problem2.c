b8b470b50fab96779bbbe673989aa3986fc95a74
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-08 15:37:33 -0600

    Problem2

--- Problem2.c
+++ Problem2.c
@@ -3,7 +3,7 @@ int main()
 {
 	char input[]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};
 	
-	for( int i = 0; i <(sizeof(input)/sizeof(int)); i++)
+	for( int i = 0; i <(sizeof(input)/sizeof(char)); i++)
 	{
 		if( input[i] >= 'A' && input[i] <= 'Z' )                                           		//http://www.asciitable.com/ for checking ASCII range
 			printf("Code: %d	Type:Upper Case 	ASCII Code: %c\n",input[i],input[i]);
