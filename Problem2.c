33f8834f9e19f491245c92685a4c477912c7ffc1
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-08 15:34:40 -0600

    Problem2

--- Problem2.c
+++ Problem2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int input[]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};
+	char input[]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};
 	
 	for( int i = 0; i <(sizeof(input)/sizeof(int)); i++)
 	{
-		if( input[i] >= 65 && input[i] <=90 )                                           		//http://www.asciitable.com/ for checking ASCII range
+		if( input[i] >= 'A' && input[i] <= 'Z' )                                           		//http://www.asciitable.com/ for checking ASCII range
 			printf("Code: %d	Type:Upper Case 	ASCII Code: %c\n",input[i],input[i]);
 		
-		else if( input[i] >= 97 && input[i] <=122 )
+		else if( input[i] >= 'a' && input[i] <= 'z' )
 			printf("Code: %d	Type:Lower Case		ASCII Code: %c\n",input[i],input[i]);
 			
-		else if( input[i] >= 48 && input[i] <=57 )
+		else if( input[i] >= '0' && input[i] <= '9' )
 			printf("Code: %d	Type:Digit		ASCII Code: %c\n",input[i],input[i]);
 		
 	}
