691cd4a8bf0f9752b276e80726823ae90cfcce51
Prayag Desai <prayag2010@live.com>
2019-09-08 02:31:34 -0600

    Can input different radix now

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct a {
+	int value;
+	int radix;
+	int opSize;
+} datastructure[11];
+
+
 int main()
 {
-    struct a{
-        int value;
-        int base;
-        int radix;
-    }datastructure[11];
-    char *ptr;
-    char test[]="{-6, 10, 4}, {-6, 9, 4}, {-6, 10, 5}, {-9, 10, 4}, {237, 10, 8}, {0354, 8, 8}, {78, 16, 8}, {-125, 10, 8},  {65400, 10, 8}, {65400, 10, 16}, {-32701, 10, 16} ";
-    ptr=test;
-    int j=0;
-    for ( int i=0; i<strlen(test); ++i)
-    {
-        if (test[i]=='{')
-        {
-            sscanf(ptr+i,"{%d,%d,%d}",&datastructure[j].value,&datastructure[j].base,&datastructure[j].radix);
-            j++;
-        }
-    }
-    printf("%d\n",j);
-    for ( int i=0; i<11; i++ )
-      printf("%d %d %d ", datastructure[i].value,datastructure[i].base,datastructure[i].radix);
-    return 0;
+	char *ptr;
+	char input[] = "{-6, 10, 4}, {-6, 9, 4}, {-6, 10, 5}, {0xff, 10, 4}, {237, 10, 8}, {0354, 8, 8}, {78, 16, 8}, {-125, 10, 8},  {65400, 10, 8}, {65400, 10, 16}, {-32701, 10, 16} ";
+	ptr = input;
+
+	int inputQuantity = 0;
+	for ( int i = 0; i < strlen(input); ++i)
+	{
+		if (input[i] == '{')
+		{
+			sscanf(ptr + i, "{%d,%d,%d}", &datastructure[inputQuantity].value, &datastructure[inputQuantity].radix, &datastructure[inputQuantity].opSize);
+
+			if(datastructure[inputQuantity].value == 0 && datastructure[inputQuantity].radix == 0 && datastructure[inputQuantity].opSize == 0)
+				sscanf(ptr + i, "{%x,%d,%d}", &datastructure[inputQuantity].value, &datastructure[inputQuantity].radix, &datastructure[inputQuantity].opSize);
+
+			if(datastructure[inputQuantity].radix == 8)
+				sscanf(ptr + i, "{%o,%d,%d}", &datastructure[inputQuantity].value, &datastructure[inputQuantity].radix, &datastructure[inputQuantity].opSize);
+
+			inputQuantity++;
+		}
+	}
+
+	printf("%d\n", inputQuantity);
+	for ( int i = 0; i < 11; i++ )
+		printf("%d %d %d ", datastructure[i].value, datastructure[i].radix, datastructure[i].opSize);
+
+	return 0;
 }
