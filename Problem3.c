c74b3718ca9d2cc4351bafec4faaf638fd7e56fe
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-09 17:42:50 -0600

    Problem3

--- Problem3.c
+++ Problem3.c
@@ -4,33 +4,40 @@ int main()
 {
     int Input = 0xCAFE;
     printf("%x\n",Input);
-	int temp=0;
+    int temp=0;
+
     if(Input & 7 == 7)
-        printf("True    %d\n",(Input & 7));
+        printf("True\t%d\n",(Input & 7));
     else
-        printf("False    %d\n",(Input & 7));
-    temp = Input & 255;
+        printf("False\t%d\n",(Input & 7));
+
+    temp = Input & 0xFF;
     Input = Input >> 8;
     Input = Input | (temp << 8);
     printf("%x\n",Input);
+
     if(Input & 7 == 7)
-        printf("True    %d\n",(Input & 7));
+        printf("True\t%d\n",(Input & 7));
     else
-        printf("False    %d\n",(Input & 7));
-    temp = Input & 15;
+        printf("False\t%d\n",(Input & 7));
+
+    temp = Input & 0x0F;
     Input = Input >> 4;
     Input = Input | (temp << 12);
     printf("%x\n",Input);
+
     if(Input & 7 == 7)
-        printf("True    %d\n",(Input & 7));
+        printf("True\t%d\n",(Input & 7));
     else
-        printf("False    %d\n",(Input & 7));
-    temp = Input & 255;
+        printf("False\t%d\n",(Input & 7));
+
+    temp = Input & 0xFF;
     Input = Input >> 8;
     Input = Input | (temp << 8);
-	printf("%x\n",Input);
-	if(Input & 7 == 7)
-        printf("True    %d\n",(Input & 7));
+    printf("%x\n",Input);
+    
+    if(Input & 7 == 7)
+        printf("True\t%d\n",(Input & 7));
     else
-        printf("False    %d\n",(Input & 7));
+        printf("False\t%d\n",(Input & 7));
 }
