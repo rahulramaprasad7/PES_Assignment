81fccb17e728fcd9566f389f294605355aeebed7
Prayag Desai <prayag2010@live.com>
2019-09-08 22:58:02 -0600

    Added Binary, oct and hex conversion

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 struct a {
@@ -8,6 +8,10 @@ struct a {
 	int opSize;
 } datastructure[11];
 
+bool negative = false;
+int operand = 0, min = 0, max = 0;
+
+void printBIN(int input, int opSize);
 
 int main()
 {
@@ -36,5 +40,68 @@ int main()
 	for ( int i = 0; i < 11; i++ )
 		printf("%d %d %d ", datastructure[i].value, datastructure[i].radix, datastructure[i].opSize);
 
+	printf("\n");
+
+	int value = datastructure[0].value;
+	int radix = datastructure[0].radix;
+	int opSize = datastructure[0].opSize;
+
+	if(value < 0){
+		negative = true;
+		operand = -1 * value;
+	} else {
+		operand = value;
+	}
+
+	/* BIN */
+	printBIN(operand, opSize);
+	//max
+	for (int i = opSize - 1; i >= 0; i--)
+	{
+		max += (1 << i);
+	}
+	printBIN(max, opSize);
+	printBIN(0, opSize);
+
+	printf("\n");
+
+	/* OCTAL */
+	printf("0%o\t", operand);
+	printf("0%o\t", max);
+	printf("0%o\t", 0);
+
+	printf("\n");
+
+    /* DECIMAL */
+	printf("%d\t", operand);
+	printf("%d\t", max);
+	printf("%d\t", 0);
+
+	printf("\n");
+
+	/* HEX */
+	printf("0x%x\t", operand);
+	printf("0x%x\t", max);
+	printf("0x%x\t", 0);
+
+	printf("\n");
+
+    //converting to signed number
+	if(negative){
+		operand += 1 << (opSize - 1);
+	}
+
+	printBIN(operand, opSize);
+
 	return 0;
+}
+
+void printBIN(int input, int opSize)
+{
+	printf("0b");
+	for (int i = opSize - 1; i >= 0; i--)
+	{
+		printf("%d", (input >> i) & 0x01);
+	}
+	printf("\t");	
 }
