1a19fef4c21c6e84d5f6d31eeb4522c5556a9568
rahulramaprasad7 <54955817+rahulramaprasad7@users.noreply.github.com>
2019-09-10 21:16:48 -0600

    Update Problem2.c

--- Problem2.c
+++ Problem2.c
@@ -5,7 +5,7 @@ int32_t main()
 {
 	char input[]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};
 	
-	for( uint32_t i = 0; i <(sizeof(input)/sizeof(char)); i++)
+	for( int32_t i = 0; i <(sizeof(input)/sizeof(char)); i++)
 	{
 		if( isupper( input[i] ) )                                           		//http://www.asciitable.com/ for checking ASCII range
 			printf("Code: %d 	Type:Upper Case 	ASCII Char: %c\n",input[i],input[i]);
