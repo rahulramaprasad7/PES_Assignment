ebdd05c1dd4888b57a7702c9b3c689fb2ea63bc9
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-15 15:21:30 -0600

    Problem3.c

--- problem3/Problem3.c
+++ problem3/Problem3.c
@@ -1,16 +1,41 @@
+/*
+ * @file Problem3.c
+ * @brief Soultion for the third problem of the PES project
+ *
+ * This is a C program takes an integer input in hexadecimal format,
+ * and performs a series of byte rotation operations and 
+ * also checks if 3 of the last 4 bits are on and if true, 
+ * prints the value in binary
+ *
+ * @author Rahul Ramaprasad, Prayag Milan Desai
+ * @date September 14 2019
+ * @version 1.0
+ */
 #include <stdio.h>
 #include <stdint.h>
 
 
+/*
+ * @brief Print the binary value for a given integer
+ *
+ * Given an integer input, this function will print a 
+ * binary value of the same
+ *
+ * @param input The input integer
+ * 
+ * @return void.
+ */
 void printBin(int32_t input)
 {
-    int32_t i;
+   
     printf("The binary value is\n");
     printf("0b");
-    for (i = 15; i >= 0; i--)
+	
+    for (int32_t i = 15; i >= 0; i--)
     {
         printf("%d", (input >> i) & 0x01);
     }
+	
     printf("\n");
 }
 
@@ -28,11 +53,16 @@ int32_t main()
 
     //Test if 3 of last 4 bits are on, and print the value in binary
     printf("Are 3 of the last 4 bits on?\n");
-    if ((input & 0x07) == 0x07) {
+	//There are four possibilities where 3 of the last 4 bits are on
+    if ( ((input & 0x0F) == 0x07) || ((input & 0x0F) == 0x0B) \
+	     || ((input & 0x0F) == 0x0D) || ((input & 0x0F) == 0x0E))
+	{
         printf("True\n");
         printBin(input);
     }
-    else {
+	
+    else
+	{
         printf("False\n");
         printBin(input);
     }
@@ -49,11 +79,16 @@ int32_t main()
     //Test if 3 of last 4 bits are on, and print the value in binary
     printf("Testing if 3 of last 4 bits are on, and printing the "
            "value in binary\n");
-    if ((input & 7) == 7) {
+	//There are four possibilities where 3 of the last 4 bits are on	   
+    if ( ((input & 0x0F) == 0x07) || ((input & 0x0F) == 0x0B) \
+	     || ((input & 0x0F) == 0x0D) || ((input & 0x0F) == 0x0E))
+	{
         printf("True\n");
         printBin(input);
     }
-    else {
+	
+    else
+	{
         printf("False\n");
         printBin(input);
     }
@@ -71,11 +106,16 @@ int32_t main()
     //Test if 3 of last 4 bits are on, and print the value in binary
     printf("Testing if 3 of last 4 bits are on, and printing the "
            "value in binary\n");
-    if ((input & 7) == 7) {
+	//There are four possibilities where 3 of the last 4 bits are on	   
+    if ( ((input & 0x0F) == 0x07) || ((input & 0x0F) == 0x0B) \
+	     || ((input & 0x0F) == 0x0D) || ((input & 0x0F) == 0x0E))
+	{
         printf("True\n");
         printBin(input);
     }
-    else {
+	
+    else
+	{
         printf("False\n");
         printBin(input);
     }
@@ -93,11 +133,15 @@ int32_t main()
     //Test if 3 of last 4 bits are on, and print the value in binary
     printf("Testing if 3 of last 4 bits are on, and printing the "
            "value in binary\n");
-    if ((input & 7) == 7) {
+    if ( ((input & 0x0F) == 0x07) || ((input & 0x0F) == 0x0B) \
+	     || ((input & 0x0F) == 0x0D) || ((input & 0x0F) == 0x0E))
+	{
         printf("True\n");
         printBin(input);
     }
-    else {
+	
+    else
+	{
         printf("False\n");
         printBin(input);
     }
